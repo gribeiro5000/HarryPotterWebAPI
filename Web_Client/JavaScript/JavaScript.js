ef53bc1bf58c71de527b8c476629fa12ad0d1a80
Gabriel Ribeiro <gribeiro.gr17@gmail.com>
2020-01-12 12:49:32 -0300

    getWizzard WebClient Done

--- Web_Client/JavaScript/JavaScript.js
+++
@@ -1,9 +0,0 @@
-﻿var xhr = new XMLHttpRequest();
-
-xhr.open('Get', 'http://localhost:64197/api/wizzard', true);
-
-xhr.onload = function () {
-    console.log(this.responseText);
-};
-
-xhr.send();
